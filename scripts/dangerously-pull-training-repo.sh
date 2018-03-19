3b4be52250f38d5118ca30111f284c543b460ec4
Paul Dowman <paul@pauldowman.com>
2018-03-19 10:41:28 -0400

    Move git dirty check before prompt

--- scripts/dangerously-pull-training-repo.sh
+++ scripts/dangerously-pull-training-repo.sh
@@ -5,6 +5,11 @@ set -e # exit if any command fails
 
 BRANCHES="react-fundamentals graphql-fundamentals advanced-graphql"
 
+if [ ! -z "$(git status --porcelain)" ]; then
+  echo "Git working directory is not clean."
+  exit 1
+fi
+
 echo "⚠️ WARNING! This will do a HARD RESET and wipe out your history on:"
 for BRANCH in $BRANCHES ; do
   echo "🔥 $BRANCH"
@@ -14,11 +19,6 @@ echo "Abort now if you haven't pushed all of those branches!"
 echo "(Or get to know git reflog...)"
 echo "Press enter to continue."
 read || exit
-
-if [ ! -z "$(git status --porcelain)" ]; then
-  echo "Git working directory is not clean."
-  exit 1
-fi
 
 echo "Fetching..."
 git fetch
--- scripts/dangerously-push-training-repo.sh
+++ scripts/dangerously-push-training-repo.sh
@@ -5,6 +5,11 @@ set -e # exit if any command fails
 
 BRANCHES="react-fundamentals graphql-fundamentals advanced-graphql"
 
+if [ ! -z "$(git status --porcelain)" ]; then
+  echo "Git working directory is not clean."
+  exit 1
+fi
+
 echo "⚠️ WARNING! This will FORCE PUSH and wipe out the remote history on:"
 for BRANCH in $BRANCHES ; do
   echo "🔥 $BRANCH"
@@ -14,13 +19,9 @@ echo "Abort now if anyone else has changed those branches since you last pulled!
 echo "Press enter to continue."
 read || exit
 
-if [ ! -z "$(git status --porcelain)" ]; then
-  echo "Git working directory is not clean."
-  exit 1
-fi
-
 for BRANCH in $BRANCHES ; do
   echo "🔥 Force pushing $BRANCH..."
   git checkout $BRANCH
   git push --force origin $BRANCH
 done
+x
